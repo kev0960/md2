65f2538e7974d6a6730d9a1ddf4fa5a0f310b7e8
Jaebum Lee <kev0960@gmail.com>
2022-01-31 23:06:57 -0800

    Use the path relative to root directory.

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 fn main() {
+    let md2_path = env!("CARGO_MANIFEST_DIR");
     println!("cargo:rustc-flags=-l dylib=stdc++ -l dylib=zmq -l dylib=fmt -l dylib=ssl -l dylib=crypto -l dylib=pqxx -l dylib=pq");
-    println!("cargo:rustc-link-search=/home/jaebum/md2/build/lib");
-    println!("cargo:rerun-if-changed=/home/jaebum/md2/build/lib");
+    println!("{}", format!("cargo:rustc-link-search={}/build/lib", md2_path));
+    println!("{}", format!("cargo:rerun-if-changed={}/build/lib", md2_path));
 }
