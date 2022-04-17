9275bdf85fa590e651f0b79dc6f4313558940f4f
Jaebum Lee <kev0960@gmail.com>
2022-04-17 14:00:34 -0700

    Fix build.rs lib searching path.

--- build.rs
+++ build.rs
@@ -1,6 +1,10 @@
 fn main() {
     let md2_path = env!("CARGO_MANIFEST_DIR");
     println!("cargo:rustc-flags=-l dylib=stdc++ -l dylib=zmq -l dylib=fmt -l dylib=ssl -l dylib=crypto -l dylib=pqxx -l dylib=pq");
+
+    // Sometimes md2_path points to host's cargo manifest dir, rather then container's dir.
+    // TODO Figure out why this is happening.
+    println!("{}", format!("cargo:rustc-link-search=/MathTeacher/md2/build/lib"));
     println!("{}", format!("cargo:rustc-link-search={}/build/lib", md2_path));
     println!("{}", format!("cargo:rerun-if-changed={}/build/lib", md2_path));
 }
